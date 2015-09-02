b04d4db473eb484b36a6d435e8d5fb6272b1acde
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2015-09-02 17:27:24 -0300

    removed gson builder

--- src/main/java/ar/edu/tareas/Main.java
+++ src/main/java/ar/edu/tareas/Main.java
@@ -4,14 +4,12 @@ import spark.Spark;
 import ar.edu.tareas.controller.TareasController;
 import ar.edu.tareas.controller.util.JsonTransformer;
 
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+		Gson gson = new Gson();
 		JsonTransformer jsonTransformer = new JsonTransformer(gson);
 
 		Spark.port(9000);
